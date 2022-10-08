3b96a22025c34a3c6316c9235ba5e462a40723ea
William Martin <maartz@icloud.com>
2022-10-08 22:17:59 +0200

    add new post on discussion show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # resources :discussions, only: %i[index create new]
-  resources :discussions
+  resources :discussions, only: %i[index show new create edit update destroy] do
+    resources :posts, only: %i[create], module: :discussions
+  end
   root to: 'main#index'
 end

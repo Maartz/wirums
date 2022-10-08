1e2e83e40e5153767975d01dd1f6ede5864d0fb1
William Martin <maartz@icloud.com>
2022-10-08 16:08:30 +0200

    add discussion creation + form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :discussions, only: [:index]
+  # resources :discussions, only: %i[index create new]
+  resources :discussions, only: [:index, :create, :new]
   root to: 'main#index'
 end

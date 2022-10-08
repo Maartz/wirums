3e207e772dff57568840f440e47d53e38768fd9c
William Martin <maartz@icloud.com>
2022-10-08 16:29:47 +0200

    edit discussion

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # resources :discussions, only: %i[index create new]
-  resources :discussions, only: [:index, :create, :new]
+  resources :discussions, only: [:index, :create, :new, :edit, :update]
   root to: 'main#index'
 end

859ff17b2b2fc45b88e646061f682819e6c33477
William Martin <maartz@icloud.com>
2022-10-08 20:45:51 +0200

    setup post model and add post count to discussions

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,11 +1,14 @@
 class Discussion < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
 
+  has_many :posts, dependent: :destroy
+
   after_create_commit -> { broadcast_prepend_to 'discussions' }
   after_update_commit -> { broadcast_replace_to 'discussions' }
   after_destroy_commit -> { broadcast_remove_to 'discussions' }
 
   validates :name, presence: true
+
 
   def to_param
     "#{id}-#{name.downcase.to_s[0...100]}".parameterize

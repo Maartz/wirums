36c5b5a280ee81287832fcba0324fac2bc4f1e36
William Martin <maartz@icloud.com>
2022-10-09 12:14:01 +0200

    realtime udpate of posts on discussion page

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,8 @@ class Post < ApplicationRecord
   has_rich_text :body
 
   validates :body, presence: true
+
+  after_create_commit lambda {
+    broadcast_append_to discussion, partial: 'discussions/posts/post', locals: { post: self }
+  }
 end

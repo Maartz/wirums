1d09bfd25b95de692bea350bf896ed6e50f5669a
William Martin <maartz@icloud.com>
2022-10-18 10:20:05 +0200

    add categories to discussions index

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,8 @@ class Category < ApplicationRecord
   has_many :discussions, dependent: :nullify
 
   scope :sorted, -> { order(name: :asc) }
+
+  after_create_commint -> { broadcast_prepend_to 'categories' }
+  after_update_commint -> { broadcast_replace_to 'categories' }
+  after_destroy_commint -> { broadcast_remove_to 'categories' }
 end
